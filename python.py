7c69f0dd79d8016189bb075ace8c804b77ba94f8
Quentin <47141399+pepequentin@users.noreply.github.com>
2023-05-02 09:14:12 +0200

    Comment dans le code

--- python.py
+++ python.py
@@ -25,40 +25,43 @@ for i in range(0, number_of_page):
     if response_json.get("success") == False and response_json.get("cause") == "Page not found":
         break
     responses.append(response_json)
+# Crée une liste d'enchères en filtrant les enchères à partir de réponses.
+# Si aucune enchère n'est trouvée, renvoie une liste vide.
 auctions = [auction for response in responses for auction in response.get("auctions", [])]
-items = []
+
+# Crée des variables pour les noms d'objets recherchés.
 item_final_destination = "Final"
 item_shadow_assassin = "Ancient Shadow Assassin"
 item_tiger = "Tiger"
 item_skeleton = "Wither Skeleton"
 item_helmet_tar = "Tarantula Helmet"
+
+# Parcours chaque enchère pour les objets recherchés.
 for auction in auctions:
     try:
-        #item_final_destination
+        # Recherche l'objet "Final Destination" pour des enchères légendaires et mythiques à bas prix.
         if auction["bin"] and auction["item_name"].find(item_final_destination) != -1:
-            # ici le prix que jachete environ 10m je vends apres recomb autour de 21m donc environ 3m de benef
+            # L'objet "Final Destination" a une enchère initiale de 15M à 22M.
             if auction["tier"] == "LEGENDARY" and auction["item_lore"].find("50,000") != -1 and auction["starting_bid"] < 14999999:
                 print("Potential 50 000 leg        : " + auction["item_name"] + " at the price of " + str(auction["starting_bid"]))
-            # ici le prix que je vends les mythic final
-            # helmet 23m
-            # chest  24m
-            # leggi  23m
-            # boots  22m
             elif auction["tier"] == "MYTHIC" and auction["item_lore"].find("50,000") != -1 and auction["starting_bid"] < 27999999:
                 print("Potential 50 000 mythic     : " + auction["item_name"] + " at the price of " + str(auction["starting_bid"]))
             elif auction["tier"] == "MYTHIC" and auction["item_lore"].find("100,000") != -1 and auction["starting_bid"] < 35000000:
                 print("Potential 100 000 mythic    : " + auction["item_name"] + " at the price of " + str(auction["starting_bid"]))
-        #item_tiger
+
+        # Recherche l'objet "Tiger" pour des enchères épiques et légendaires à bas prix.
         elif auction["bin"] and auction["item_name"].find(item_tiger) != -1:
             if auction["tier"] == "EPIC" and auction["item_name"].find("Lvl 100") != -1 and auction["starting_bid"] < 18000000:
                 print("Tiger epic lvl 100          : " + str(auction["starting_bid"]))
             elif auction["tier"] == "LEGENDARY" and auction["item_name"].find("Lvl 100") != -1 and auction["starting_bid"] < 35000000:
                 print("Tiger leg lvl 100           : " + str(auction["starting_bid"]))
-        #item_skeleton
+
+        # Recherche l'objet "Wither Skeleton" pour des enchères légendaires à bas prix.
         elif auction["bin"] and auction["item_name"].find(item_skeleton) != -1:
             if auction["tier"] == "LEGENDARY" and auction["item_name"].find("Lvl 100") != -1 and auction["starting_bid"] < 9000000:
                 print("Wither skeleton leg lvl 100 : " + str(auction["starting_bid"]))
-        #item_helmet_tar
+
+        # Recherche l'objet "Tarantula Helmet" pour des enchères mythiques à bas prix.
         elif auction["bin"] and auction["item_name"].find(item_helmet_tar) != -1:
             if auction["tier"] == "MYTHIC" and auction["item_lore"].find("25,000") != -1 and auction["starting_bid"] < 20000000:
                 print("Potential helmet tarantula : " + str(auction["starting_bid"]))
